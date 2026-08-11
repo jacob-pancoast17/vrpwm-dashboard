f520d740c51b9b53f3e446ebe3328512a1831fbc
Jacob <jacob.pancoast17@gmail.com>
2026-08-11 19:55:12 -0400

    Add disease selector input (not functional) and load RDS for other available CDC wastewater viruses

--- data/fetch.R
+++ data/fetch.R
@@ -2,9 +2,20 @@ pacman::p_load(readr, sf)
 
 dir.create("data")
 
-data_unfiltered <- read_csv("https://data.cdc.gov/api/v3/views/45cq-cw4i/query.csv")
 geography <- st_read("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/arcgis/rest/services/FS_VCGI_OPENDATA_Boundary_BNDHASH_poly_counties_SP_v1/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson", quiet = TRUE) |>
     st_transform(4326)  
 
-saveRDS(data_unfiltered, "data/nwss.rds")
+rsv <- read_csv("https://data.cdc.gov/api/v3/views/45cq-cw4i/query.csv")
+measles <- read_csv("https://data.cdc.gov/api/v3/views/akvg-8vrb/query.csv")
+covid <- read_csv("https://data.cdc.gov/api/v3/views/j9g8-acpt/query.csv")
+flu_a <- read_csv("https://data.cdc.gov/api/v3/views/ymmh-divb/query.csv")
+mpox <- read_csv("https://data.cdc.gov/api/v3/views/xpxn-rzgz/query.csv")
+
+
 saveRDS(geography, "data/geography.rds")
+
+saveRDS(rsv, "data/rsv.rds")
+saveRDS(measles, "data/measles.rds")
+saveRDS(covid, "data/covid.rds")
+saveRDS(flu_a, "data/flu_a.rds")
+saveRDS(mpox, "data/mpox.rds")
