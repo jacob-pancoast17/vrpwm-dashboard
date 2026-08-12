e46175008f451f7ef570e477073608706bc735e4
Jacob <jacob.pancoast17@gmail.com>
2026-08-12 12:48:14 -0400

    Changed palette to a reactive container for diff. colors per disease and removed measles + mpox due to insufficient data

--- data/fetch.R
+++ data/fetch.R
@@ -6,16 +6,11 @@ geography <- st_read("https://services1.arcgis.com/BkFxaEFNwHqX3tAw/arcgis/rest/
     st_transform(4326)  
 
 rsv <- read_csv("https://data.cdc.gov/api/v3/views/45cq-cw4i/query.csv")
-measles <- read_csv("https://data.cdc.gov/api/v3/views/akvg-8vrb/query.csv")
 covid <- read_csv("https://data.cdc.gov/api/v3/views/j9g8-acpt/query.csv")
 flu_a <- read_csv("https://data.cdc.gov/api/v3/views/ymmh-divb/query.csv")
-mpox <- read_csv("https://data.cdc.gov/api/v3/views/xpxn-rzgz/query.csv")
-
 
 saveRDS(geography, "data/geography.rds")
 
 saveRDS(rsv, "data/rsv.rds")
-saveRDS(measles, "data/measles.rds")
 saveRDS(covid, "data/covid.rds")
 saveRDS(flu_a, "data/flu_a.rds")
-saveRDS(mpox, "data/mpox.rds")
